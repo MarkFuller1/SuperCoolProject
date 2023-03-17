4a064bd04d8bc90783dcf84134fd43bc6bc736e7
xslop <xslopez2002@gmail.com>
2023-03-17 11:50:31 -0500

    Xavier Lopez says Hello World!

---
+++ main.py
@@ -0,0 +1,11 @@
+# Author: Xavier Lopez
+# Date Started: 5/18/2022
+# Date Finished: 5/18/2022
+
+#
+# Description:
+# Write a Python program that will print "Hello World!"
+#   to the screen
+#
+
+print("Hello World!")
